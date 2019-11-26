d5392666352e06a5d0a1867ad8405284c789efee
KonradJPE <ktomasik@jpembedded.eu>
2019-11-26 12:19:48 +0100

    Remove '(c)' from copyright line

--- sysmanagbus_portcb.c
+++ sysmanagbus_portcb.c
@@ -19,7 +19,7 @@
  *               MEN_EP02      - to provide additional routines for the EP02
  *
  *---------------------------------------------------------------------------
- * Copyright (c) 1998-2019, MEN Mikro Elektronik GmbH
+ * Copyright 1998-2019, MEN Mikro Elektronik GmbH
  ******************************************************************************/
 /*
  * This program is free software: you can redistribute it and/or modify
